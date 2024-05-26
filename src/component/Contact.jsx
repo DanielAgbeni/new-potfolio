c6f5b00ca44977a0375785f006236d0f0600421e
Daniel Agbeni <99095841+DanielAgbeni@users.noreply.github.com>
2024-05-26 11:28:01 +0100

    "Updated Contact component with WhatsApp link and form submission, refactored Header component with menu functionality and styling changes."

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -13,10 +13,20 @@ import { MdClose, MdEmail, MdLocationCity, MdSend } from 'react-icons/md';
 import { motion } from 'framer-motion';
 
 const Contact = ({ click }) => {
-	const handleSubmit = (e) => {};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const name = e.target.name.value;
+		const email = e.target.email.value;
+		const reason = e.target.reason.value;
+		const message = `Name: ${name}\nEmail: ${email}\nReason: ${reason}`;
+		const whatsappLink = `https://wa.me/message/CU3TXS5F4LYXA1?text=${encodeURIComponent(
+			message,
+		)}`;
+		window.location.href = whatsappLink;
+	};
 
 	return (
-		<div className='absolute w-[90%] max-w-[800px] flex flex-col gap-3 p-10 -translate-x-1/2 backdrop-blur-xl bg-gradient-to-br from-gray-600 to-sky-600 bg-opacity-90  shadow-white top-20 rounded-md left-1/2'>
+		<div className='absolute w-[90%] max-w-[800px] flex flex-col gap-3 p-10 -translate-x-1/2 backdrop-blur-xl bg-gradient-to-br from-gray-600 to-sky-600 bg-opacity-90 shadow-white top-20 rounded-md left-1/2'>
 			<div className='flex justify-end'>
 				<button onClick={() => click(false)}>
 					<MdClose className='text-red-500 text-xl' />
@@ -32,60 +42,63 @@ const Contact = ({ click }) => {
 							<div className='w-full order-2 relative h-auto'>
 								<input
 									type='text'
+									name='name'
 									placeholder='Your Name'
-									className='w-full py-3  drop-shadow-none border  bg-sky-800  px-3 pr-3 pl-3 outline-none rounded-md shadow-none '
+									className='w-full py-3 drop-shadow-none border bg-sky-800 px-3 pr-3 pl-3 outline-none rounded-md shadow-none'
 								/>
 							</div>
 						</div>
 						<div className='w-full relative flex flex-col'>
 							<div className='w-full order-2 relative h-auto'>
 								<input
-									id=''
-									type='text'
+									type='email'
+									name='email'
 									placeholder='Your email'
-									class='w-full py-3  drop-shadow-none border bg-sky-800  px-3 pr-3 pl-3 outline-none rounded-md shadow-none '
+									className='w-full py-3 drop-shadow-none border bg-sky-800 px-3 pr-3 pl-3 outline-none rounded-md shadow-none'
 								/>
 							</div>
 						</div>
 						<div className='w-full relative flex flex-col'>
-							<div class='w-full order-2 relative h-auto  '>
+							<div className='w-full order-2 relative h-auto'>
 								<input
-									id=''
 									type='text'
+									name='reason'
 									placeholder='Reason for message'
-									class='w-full py-3  drop-shadow-none border bg-sky-800   px-3 pr-3 pl-3 outline-none rounded-md shadow-none '
+									className='w-full py-3 drop-shadow-none border bg-sky-800 px-3 pr-3 pl-3 outline-none rounded-md shadow-none'
 								/>
 							</div>
 						</div>
 						<div className='w-full relative flex flex-col'>
-							<div class='w-full order-2 relative h-auto  '>
+							<div className='w-full order-2 relative h-auto'>
 								<textarea
-									id=''
+									name='message'
 									placeholder='Message...'
-									class='w-full drop-shadow-none border bg-sky-800  py-3 px-3 outline-none rounded-md shadow-none h-[100px]'></textarea>
+									className='w-full drop-shadow-none border bg-sky-800 py-3 px-3 outline-none rounded-md shadow-none h-[100px]'></textarea>
 							</div>
 						</div>
-						<button className='button-hover inline-flex py-2 px-5 rounded-full font-medium bg-blue-700 text-white items-center justify-center gap-2 text-center'>
+						<button
+							className='button-hover inline-flex py-2 px-5 rounded-full font-medium bg-blue-700 text-white items-center justify-center gap-2 text-center cursor-pointer active:scale-95'
+							type='submit'>
 							<span>Send</span>
 							<MdSend className='text-lg hover:scale-110' />
 						</button>
 					</form>
 				</div>
-				<div class='relative md:flex hidden items-center justify-center w-full overflow-hidden sm:w-auto'>
-					<p class='font-bold text-gray-400 after:absolute before:absolute after:bg-gray-800  before:bg-gray-800  after:h-[2px] after:w-full before:h-[2px] before:w-full after:top-1/2 after:-translate-y-1/2 before:bottom-1/2 before:-translate-y-1/2 after:-left-[60%] before:-right-[60%] sm:after:translate-y-[unset] sm:before:translate-y-[unset] sm:after:h-full sm:after:-top-[55%] sm:after:left-1/2 sm:after:-translate-x-1/2 sm:after:w-[2px] sm:before:h-full sm:before:-bottom-[55%] sm:before:left-1/2 sm:before:-translate-x-1/2 sm:before:w-[2px] '>
+				<div className='relative md:flex hidden items-center justify-center w-full overflow-hidden sm:w-auto'>
+					<p className='font-bold text-gray-400 after:absolute before:absolute after:bg-gray-800 before:bg-gray-800 after:h-[2px] after:w-full before:h-[2px] before:w-full after:top-1/2 after:-translate-y-1/2 before:bottom-1/2 before:-translate-y-1/2 after:-left-[60%] before:-right-[60%] sm:after:translate-y-[unset] sm:before:translate-y-[unset] sm:after:h-full sm:after:-top-[55%] sm:after:left-1/2 sm:after:-translate-x-1/2 sm:after:w-[2px] sm:before:h-full sm:before:-bottom-[55%] sm:before:left-1/2 sm:before:-translate-x-1/2 sm:before:w-[2px]'>
 						OR
 					</p>
 				</div>
-				<div class='w-full sm:w-[40%] md:flex hidden flex-col gap-10'>
-					<h1 class='font-bold text-gray-200 text-sm'>Contact Me</h1>
-					<ul class='flex flex-col gap-4 text-gray-400'>
+				<div className='w-full sm:w-[40%] md:flex hidden flex-col gap-10'>
+					<h1 className='font-bold text-gray-200 text-sm'>Contact Me</h1>
+					<ul className='flex flex-col gap-4 text-gray-400'>
 						<li>
 							<a
 								href='mailto:danielagbeni12@gmail.com'
 								target='_blank'
 								rel='noreferrer'
-								class='flex items-center gap-2 duration-300 hover:pl-2'>
-								<span class='text-primary'>
+								className='flex items-center gap-2 duration-300 hover:pl-2'>
+								<span className='text-primary'>
 									<MdEmail className=' w-6 h-6' />
 								</span>
 								<span className='text-white'>danielagbeni12@gmail.com</span>
@@ -96,78 +109,78 @@ const Contact = ({ click }) => {
 								href=''
 								target='_blank'
 								rel='noreferrer'
-								class='flex items-center gap-2 duration-300 hover:pl-2'>
-								<span class='text-primary'>
+								className='flex items-center gap-2 duration-300 hover:pl-2'>
+								<span className='text-primary'>
 									<MdLocationCity className=' w-6 h-6' />
 								</span>
 								<span className='text-white'>Lagos State Nigeria</span>
 							</a>
 						</li>
 					</ul>
-					<ul class='flex flex-wrap items-center gap-6 text-primary-700 gap-x-4 text-gray-400'>
-						<li class='text-lg md:text-xl'>
+					<ul className='flex flex-wrap items-center gap-6 text-primary-700 gap-x-4 text-gray-400'>
+						<li className='text-lg md:text-xl'>
 							<a
 								title='Follow me on X'
 								href='https://twitter.com/Agbeni_Daniel'
 								aria-label='Follow me on X'
 								target='_blank'
-								rel='norefferer'
-								class='inline-flex hover:scale-110 duration-300 hover:text-white'>
+								rel='noreferrer'
+								className='inline-flex hover:scale-110 duration-300 hover:text-white'>
 								<FaTwitter className=' w-7 h-7' />
 							</a>
 						</li>
-						<li class='text-lg md:text-xl'>
+						<li className='text-lg md:text-xl'>
 							<a
 								title='Subscribe to my channel'
 								href='https://youtube.com/@danielagbeni'
 								aria-label='Subscribe to my channel'
 								target='_blank'
-								rel='norefferer'
-								class='inline-flex hover:scale-110duration-300 hover:text-white'>
+								rel='noreferrer'
+								className='inline-flex hover:scale-110 duration-300 hover:text-white'>
 								<FaYoutube className='w-7 h-7' />
 							</a>
 						</li>
-						<li class='text-lg md:text-xl'>
+						<li className='text-lg md:text-xl'>
 							<a
 								title='Follow me on Instagram'
 								href='https://instagram.com/daniel_agbeni'
 								aria-label='Follow me on Instagram'
 								target='_blank'
-								rel='norefferer'
-								class='inline-flex hover:scale-110 duration-300 hover:text-white'>
+								rel='noreferrer'
+								className='inline-flex hover:scale-110 duration-300 hover:text-white'>
 								<FaInstagram className='w-7 h-7 ' />
 							</a>
 						</li>
-						<li class='text-lg md:text-xl'>
+						<li className='text-lg md:text-xl'>
 							<a
 								title='Follow me on Github'
 								href='https://github.com/DanielAgbeni'
 								aria-label='Follow me on Github'
 								target='_blank'
-								rel='norefferer'
-								class='inline-flex hover:scale-110 duration-300 hover:text-white'>
+								rel='noreferrer'
+								className='inline-flex hover:scale-110 duration-300 hover:text-white'>
 								<FaGithub className='w-7 h-7' />
 							</a>
 						</li>
-						<li class='text-lg md:text-xl'>
+						<li className='text-lg md:text-xl'>
 							<a
 								title='Follow me on Discord'
 								href='https://discordapp.com/users/daniel_agbeni'
 								aria-label='Follow me on Discord'
 								target='_blank'
-								rel='norefferer'
-								class='inline-flex hover:scale-110 duration-300 hover:text-white'>
+								rel='noreferrer'
+								className='inline-flex hover:scale-110 duration-300 hover:text-white'>
 								<FaDiscord className='w-7 h-7' />
 							</a>
 						</li>
-						<li class='text-lg md:text-xl'>
+						<li className='text-lg md:text-xl'>
 							<a
 								title='Message me on Whatsapp'
 								href='https://wa.me/+2349041995875'
 								aria-label='Message me on Whatsapp'
 								target='_blank'
-								rel='norefferer'
-								class='inline-flex hover:scale-110 duration-300 hover:text-white'>
+								rel='noreferrer'
+								className='inline-flex hover:scale-110 duration-300 hover:text-white'>
 								<FaWhatsapp className='w-7 h-7' />
 							</a>
 						</li>
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,7 @@
 /** @format */
 
-import React, { useState } from 'react';
-import { MdClose, MdContactPhone, MdMenu } from 'react-icons/md';
+import React, { useState, useEffect, useRef } from 'react';
+import { MdClose, MdMenu } from 'react-icons/md';
 import { Link, useLocation } from 'react-router-dom';
 import Contact from './Contact';
 import { FaPhone } from 'react-icons/fa6';
@@ -10,12 +10,26 @@ const Header = () => {
 	const [isMenu, setIsMenu] = useState(false);
 	const [contact, setContact] = useState(false);
 	const location = useLocation();
-	const current = location.pathname === location;
+	const menuRef = useRef(null);
+
+	useEffect(() => {
+		const handleClickOutside = (event) => {
+			if (menuRef.current && !menuRef.current.contains(event.target)) {
+				setIsMenu(false);
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, []);
 
 	return (
 		<header className='fixed z-50 w-screen h-20 p-3 px-4 md:p-4 md:px-16 bg-gradient-to-br from-red-800 via-gray-700 to-sky-400 drop-shadow-xl'>
 			{/* Desktop */}
-			<div className='hidden md:flex w-full h-full items-center justify-between  shadow-white'>
+			<div className='hidden md:flex w-full h-full items-center justify-between shadow-white'>
 				<p className='logo'>Daniel Agbeni</p>
 				<div className='flex items-center gap-8'>
 					<ul className='flex item-center gap-8 ml-auto'>
@@ -25,7 +39,6 @@ const Header = () => {
 									location.pathname === '/' ? 'white' : 'gray-400'
 								} duration-100 transition-all ease-in-out text-xl font-semibold`}
 								onClick={() => setContact(false)}>
-								{' '}
 								Home
 							</li>
 						</Link>
@@ -35,7 +48,6 @@ const Header = () => {
 									location.pathname === '/about' ? 'white' : 'gray-400'
 								}  duration-100 transition-all ease-in-out text-xl font-semibold`}
 								onClick={() => setContact(false)}>
-								{' '}
 								About
 							</li>
 						</Link>
@@ -45,7 +57,6 @@ const Header = () => {
 									location.pathname === '/service' ? 'white' : 'gray-400'
 								}  duration-100 transition-all ease-in-out text-xl font-semibold`}
 								onClick={() => setContact(false)}>
-								{' '}
 								Service
 							</li>
 						</Link>
@@ -55,15 +66,13 @@ const Header = () => {
 									location.pathname === '/potfolio' ? 'white' : 'gray-400'
 								}  duration-100 transition-all ease-in-out text-xl font-semibold `}
 								onClick={() => setContact(false)}>
-								{' '}
 								Potfolio
 							</li>
 						</Link>
 
 						<li
-							className={`cursor-pointer  hover:text-white text-gray-400  duration-100 transition-all ease-in-out text-xl font-semibold flex items-center justify-center rounded-full py-3 px-6 bg-blue-600 hover:bg-blue-400`}
+							className='cursor-pointer  hover:text-white text-gray-400  duration-100 transition-all ease-in-out text-xl font-semibold flex items-center justify-center rounded-full py-3 px-6 bg-blue-600 hover:bg-blue-400'
 							onClick={() => setContact(!contact)}>
-							{' '}
 							<FaPhone />
 						</li>
 					</ul>
@@ -83,44 +92,40 @@ const Header = () => {
 					)}
 
 					{isMenu && (
-						<div className='w-40 bg-sky-800 shadow-xl rounded-lg flex flex-col absolute top-8 -right-1 py-3 px-3'>
+						<div
+							ref={menuRef}
+							className='w-64 bg-gray-800 shadow-xl rounded-lg flex flex-col absolute top-12 right-0 py-3 px-3 z-50'>
 							<ul className='flex flex-col gap-1'>
 								<Link to={'/'}>
 									<li
-										className='cursor-pointer font-extrabold hover:text-white text-black duration-500 bg-blue-300 
-										current:text-blue-500 rounded-lg transition-all ease-in-out px-4 py-2 '
+										className='cursor-pointer font-extrabold hover:bg-gray-600 text-white duration-500 rounded-lg transition-all ease-in-out px-4 py-2'
 										onClick={() => setIsMenu(false)}>
 										Home
 									</li>
 								</Link>
-								<hr />
 								<Link to={'/about'}>
 									<li
-										className='cursor-pointer font-extrabold hover:text-white text-black duration-500 bg-blue-300 rounded-lg transition-all ease-in-out px-4 py-2 '
+										className='cursor-pointer font-extrabold hover:bg-gray-600 text-white duration-500 rounded-lg transition-all ease-in-out px-4 py-2'
 										onClick={() => setIsMenu(false)}>
 										About
 									</li>
 								</Link>
-								<hr />
 								<Link to={'/service'}>
 									<li
-										className='cursor-pointer font-extrabold hover:text-white text-black duration-500 bg-blue-300 rounded-lg transition-all ease-in-out px-4 py-2 '
+										className='cursor-pointer font-extrabold hover:bg-gray-600 text-white duration-500 rounded-lg transition-all ease-in-out px-4 py-2'
 										onClick={() => setIsMenu(false)}>
 										Service
 									</li>
 								</Link>
-								<hr />
 								<Link to={'/potfolio'}>
 									<li
-										className='cursor-pointer font-extrabold hover:text-white text-black duration-500 bg-blue-300 rounded-lg transition-all ease-in-out px-4 py-2 '
+										className='cursor-pointer font-extrabold hover:bg-gray-600 text-white duration-500 rounded-lg transition-all ease-in-out px-4 py-2'
 										onClick={() => setIsMenu(false)}>
 										Potfolio
 									</li>
 								</Link>
-								<hr />
-
 								<li
-									className='cursor-pointer font-extrabold hover:text-white text-black duration-500 bg-blue-300 rounded-lg transition-all ease-in-out px-4 py-2 '
+									className='cursor-pointer font-extrabold hover:bg-gray-600 text-white duration-500 rounded-lg transition-all ease-in-out px-4 py-2'
 									onClick={() => {
 										setIsMenu(false);
 										setContact(!contact);
@@ -131,7 +136,6 @@ const Header = () => {
 						</div>
 					)}
 				</div>
-				{}
 				{contact ? <Contact click={setContact} /> : ''}
 			</div>
 		</header>
